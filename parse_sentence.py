ded26b9229c424fe115ad5a434fff43e695adcc8
jerryxhe <rebound1618@gmail.com>
2017-08-31 18:42:53 -0400

    Create parse_sentence.py

---
+++ parse_sentence.py
@@ -0,0 +1,11 @@
+syntaxnet_root_dir = "/Users/jerryhe/dev/models/syntaxnet"
+
+import subprocess
+
+def tag(sent, _cmd = '{0}/syntaxnet/models/parsey_universal/parse.sh {0}/English'.format(syntaxnet_root_dir)):
+    read, write = os.pipe()
+    os.write(write, sent)
+    os.close(write)
+    _ans = str(subprocess.check_output(_cmd, shell=True,stdin=read))
+    os.close(read)
+    return _ans
